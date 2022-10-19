f1adc4c816f0947dcfdec71a6b9e07f11b0d0acc
Samuel <s.rodriguez2003q@gmail.com>
2022-10-19 15:03:01 -0500

    todo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,42 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'proyecto-angular';
+  constructor(){}
+
+  pantalla ="";
+  pantalla2 = "";
+
+  uno(){
+    this.pantalla = this.pantalla + "1";
+  }
+  dos(){
+    this.pantalla = this.pantalla + "2";
+  }
+  tres(){
+    this.pantalla = this.pantalla + "3";
+  }
+  cuatro(){
+    this.pantalla = this.pantalla + "4";
+  }
+  cinco(){
+    this.pantalla = this.pantalla + "5";
+  }
+  seis(){
+    this.pantalla = this.pantalla + "6";
+  }
+  siete(){
+    this.pantalla = this.pantalla + "7";
+  }
+  ocho(){
+    this.pantalla = this.pantalla + "8";
+  }
+  nueve(){
+    this.pantalla = this.pantalla + "9";
+  }
+  cero(){
+    this.pantalla = this.pantalla + "9";
+  }
+  suma(){
+    this.pantalla = this.pantalla + "+";
+  }
 }
