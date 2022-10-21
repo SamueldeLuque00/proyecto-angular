512e8573faa151f437d580c4313c68effe5f1a01
Samuel <s.rodriguez2003q@gmail.com>
2022-10-21 16:46:46 -0500

    finish

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,6 +11,8 @@ export class AppComponent {
 
   pantalla ="";
   pantalla2 = "";
+  operador = "";
+  punto = false;
 
   uno(){
     this.pantalla = this.pantalla + "1";
@@ -40,9 +42,74 @@ export class AppComponent {
     this.pantalla = this.pantalla + "9";
   }
   cero(){
-    this.pantalla = this.pantalla + "9";
+    this.pantalla = this.pantalla + "0";
   }
-  suma(){
-    this.pantalla = this.pantalla + "+";
+  decimal(){
+    if(this.punto == false){
+      this.pantalla = this.pantalla + ".";
+      this.punto = true;
+    }
+  }
+  borrarM(){
+    this.pantalla;
+    let nueva = "";
+    for(let  i=0; i<this.pantalla.length-1; i++)
+    {
+      nueva = nueva + this.pantalla[i];
+    }
+    this.pantalla = this.pantalla = nueva;
+  }
+  borrar(){
+    this.pantalla = "";
+    this.pantalla2 = "";
+  }
+  borrarP(){
+    this.pantalla = "";
+  }
+  sumar(){
+    this.pantalla2 = this.pantalla;
+    this.pantalla="";
+    this.operador="sumar";
+    this.punto = false;
+  }
+  restar(){
+    this.pantalla2 = this.pantalla;
+    this.pantalla = "";
+    this.operador = "restar"
+    this.punto = false;
+  }
+  dividir(){
+    this.pantalla2 = this.pantalla;
+    this.pantalla = "";
+    this.operador = "dividir";
+    this.punto = false;
+  }
+  multiplicar(){
+    this.pantalla2 = this.pantalla;
+    this.pantalla = "";
+    this.operador = "multiplicar"
+    this.punto = false;
+  }
+  resultado(){
+    let valor1 = Number(this.pantalla);
+    let valor2 = Number(this.pantalla2);
+
+    if(this.operador == "sumar"){
+      let suma = Number(valor1) + Number(valor2);
+      this.pantalla = suma.toString();
+      this.punto = false;
+    }else if(this.operador == "restar"){
+      let resta = Number(valor1) - Number(valor2);
+      this.pantalla = resta.toString();
+      this.punto = false;
+    }else if(this.operador == "multiplicar"){
+      let multiplica = Number(valor1) * Number(valor2);
+      this.pantalla = multiplica.toString();
+      this.punto = false;
+    }else if(this.operador == "dividir"){
+      let divide = Number(valor1) / Number(valor2);
+      this.pantalla = divide.toString();
+      this.punto = false;
+    }
   }
 }
